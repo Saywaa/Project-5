f5ee4c1e713e93afef97e6bb7d9fca4068a25313
Alexandre <alexandre232003@hotmail.com>
2023-08-21 21:53:45 +0200

    Ajout des fonctions pour calculer le prix, modifier la quantité et supprimer un produit dans la page panier

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,9 @@
 let localStorageProducts = JSON.parse(localStorage.getItem("Panier"))
+let i = 0
 
-let i = 0;
+if ((localStorageProducts) === null) {
+    document.querySelector('.cart__price').innerHTML = `<p>Total (<span id="totalQuantity">0</span> articles) : <span id="totalPrice">0</span> €</p>`
+}
 
 for (product of localStorageProducts) {
     fetch('http://localhost:3000/api/products/' + product.id)
@@ -8,10 +11,10 @@ for (product of localStorageProducts) {
         return res.json()
     })
     .then((dataProduct) => {
-        localStorageProducts[i].imageUrl = dataProduct.imageUrl;
-        localStorageProducts[i].altTxt = dataProduct.altTxt;
-        localStorageProducts[i].name = dataProduct.name;
-        localStorageProducts[i].price = dataProduct.price;
+        localStorageProducts[i].imageUrl = dataProduct.imageUrl
+        localStorageProducts[i].altTxt = dataProduct.altTxt
+        localStorageProducts[i].name = dataProduct.name
+        localStorageProducts[i].price = dataProduct.price
         document.querySelector('#cart__items').innerHTML += `<article class="cart__item" data-id= ${localStorageProducts[i].id}  data-color= ${localStorageProducts[i].color}>
         <div class="cart__item__img">
             <img src=${localStorageProducts[i].imageUrl} alt=${localStorageProducts[i].altTxt}>
@@ -33,7 +36,61 @@ for (product of localStorageProducts) {
             </div>
         </div>
     </article>`
-    i++;
+    modifQuantity()
+    deleteProduct()
+    totalPriceCalcul()
+    i++
 })
 }
 
+const totalPriceCalcul = () => {
+    var totalQuantity = 0
+    var totalPrice = 0
+for (let i = 0; i < localStorageProducts.length; i++) {
+    let quantityCartProduct = localStorageProducts[i].quantity
+    totalQuantity += parseInt(quantityCartProduct)
+    let priceCartProduct = localStorageProducts[i].price * localStorageProducts[i].quantity
+    totalPrice += priceCartProduct
+    }
+    document.querySelector('.cart__price').innerHTML = `<p>Total (<span id="totalQuantity">${totalQuantity}</span> articles) : <span id="totalPrice">${totalPrice}</span> €</p>`
+}
+
+function modifQuantity () {
+    let inputQuantity = Array.from(document.querySelectorAll(".cart__item__content__settings__quantity input"))
+    let valueQuantity = Array.from(document.querySelectorAll('.itemQuantity'))
+    for (let i = 0; i < inputQuantity.length; i++) {
+        inputQuantity[i].addEventListener("change", () => {
+        uptadedLocalStorage = localStorageProducts
+        for (let i = 0; i < uptadedLocalStorage.length; i++) {
+            delete uptadedLocalStorage[i].imageUrl
+            delete uptadedLocalStorage[i].altTxt
+            delete uptadedLocalStorage[i].name
+            delete uptadedLocalStorage[i].price
+        }
+        uptadedLocalStorage[i].quantity = valueQuantity[i].value
+        localStorage.setItem("Panier", JSON.stringify(uptadedLocalStorage))
+        window.location.reload()
+        totalPriceCalcul()
+        })
+    }
+}
+
+function deleteProduct() {
+    let suppButton = Array.from(document.querySelectorAll(".deleteItem"))
+    let newLocalStorage = []
+        for (let i = 0; i < suppButton.length; i++) {
+            suppButton[i].addEventListener("click", () => {
+            suppButton[i].style.display = "none"
+            newLocalStorage = localStorageProducts
+            for (let i = 0; i < newLocalStorage.length; i++) {
+                delete newLocalStorage[i].imageUrl
+                delete newLocalStorage[i].altTxt
+                delete newLocalStorage[i].name
+                delete newLocalStorage[i].price
+            }
+            newLocalStorage.splice([i], 1)
+            localStorageProducts = localStorage.setItem("Panier", JSON.stringify(newLocalStorage))
+            window.location.reload()
+        })
+    }
+}
