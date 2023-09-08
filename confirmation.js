dea632ae81203afe191fb17fcefc3459ddd79c2d
Saywaa <90260146+Saywaa@users.noreply.github.com>
2023-09-08 17:55:07 +0200

    Delete confirmation.js

--- confirmation.js
+++
@@ -1,6 +0,0 @@
-let url = new URLSearchParams(document.location.search)
-let id = url.get("id")
-const orderId = id
-const idConfirm = document.getElementById('orderId')
-idConfirm.innerHTML = `<span id="orderId"><strong>${orderId}</strong>`
-localStorage.clear()
